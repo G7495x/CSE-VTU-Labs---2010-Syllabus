2aec1fabea1a2281ee52f8630b3d8635ee2b688a
G7495x <ghanashyam74@gmail.com>
2016-11-12 22:02:56 +0530

    Update

--- (10CSL77) Networks Lab/08 - Distance Vector/8.cpp
+++ (10CSL77) Networks Lab/08 - Distance Vector/8.cpp
@@ -2,48 +2,48 @@
 #include <cstdio>
 using namespace std;
 
-struct node{
-    int dist[20];
-    int from[20];
-} route[10];
+class node{
+public:
+	int dist[20];
+	int from[20];
+}
 
 int main(){
-    int dm[20][20],no;
+	int n,d[20][20];
+	node route[10];
 
-    cout<<"Enter no of nodes."<<'\n';
-    cin>>no;
-    cout<<"Enter the distance matrix:"<<'\n';
-    for (int i=0;i<no;i++){
-        for (int j=0;j<no;j++){
-            cin>>dm[i][j];
-            /*  Set distance from i to i as 0 */
-            dm[i][i]=0;
-            route[i].dist[j]=dm[i][j];
-            route[i].from[j]=j;
-        }
-    }
+	cout<<"Enter n of nodes:";
+	cin>>n;
+	cout<<"Enter the distance matrix:\n";
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cin>>d[i][j];
+			d[i][i]=0;					//Distance from i to i = 0
+			route[i].dist[j]=d[i][j];
+			route[i].from[j]=j;
+		}
+	}
 
-    bool flag;
-    do{
-        flag=0;
-        for (int i=0;i<no;i++){
-            for (int j=0;j<no;j++){
-                for (int k=0;k<no;k++){
-                    if ((route[i].dist[j]) > (route[i].dist[k]+route[k].dist[j])){
-                        route[i].dist[j]=route[i].dist[k]+route[k].dist[j];
-                        route[i].from[j]=k;
-                        flag=1;
-                    }
-                }
-            }
-        }
-    } while (flag);
+	//Floyd's all pair shortest path algorithm
+	bool flag;
+	do{
+		flag=0;
+		for(int i=0;i<n;i++)
+			for(int j=0;j<n;j++)
+				for(int k=0;k<n;k++)
+					if((route[i].dist[j])>(route[i].dist[k]+route[k].dist[j])){
+						route[i].dist[j]=route[i].dist[k]+route[k].dist[j];
+						route[i].from[j]=k;
+						flag=1;
+					}
+	}while(flag);
 
-    for (int i=0;i<no;i++){
-        cout<<"Router info for router: "<<i+1<<'\n';
-        cout<<"Dest\tNext Hop\tDist"<<'\n';
-        for (int j=0;j<no;j++)
-            printf("%d\t%d\t\t%d\n",j+1,route[i].from[j]+1,route[i].dist[j]);
-    }
-    return 0;
+	for(int i=0;i<n;i++){
+		cout<<"Router info for router: "<<i+1<<'\n';
+		cout<<"Dest\tNext Hop\tDist"<<'\n';
+		for(int j=0;j<n;j++)
+			printf("%d\t%d\t\t%d\n",j+1,route[i].from[j]+1,route[i].dist[j]);
+	}
+
+	return 0;
 }

fb3a66f78b92da7bc25bcfb46bee721f8e51dd26
G7495x <ghanashyam74@gmail.com>
2016-11-14 00:53:52 +0530

    Updated

---
+++ (10CSL77) Networks Lab/10 - Client Server FIFO/10.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+inline void check(int ret,char* cmd){
+	if(ret==-1){
+		perror(cmd);
+		exit(1);
+	}
+}
+
+int main(int argc,char** argv){
+	int fifo,file;
+	char filename[256],data[10240];
+	int count;
+
+	switch(argc){
+		//Server
+		case 1:		printf("SERVER\n");
+
+					check(mkfifo("FIFO",S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO),"mkfifo");
+					check(fifo=open("FIFO",O_RDONLY|O_NONBLOCK),"open");
+					printf("Waiting for client...\n");
+					while((count=read(fifo,filename,sizeof(filename)))==0)	sleep(1);
+					filename[count]='\0';
+					close(fifo);
+					unlink("FIFO");
+
+					printf("Reading file %s\n",filename);
+					check(file=open(filename,O_RDONLY),"open");
+					count=read(file,data,sizeof(data));
+					data[count]='\0';
+					close(file);
+
+					sleep(2);
+					check(fifo=open("FIFO",O_WRONLY),"open");
+					printf("Writing back to client\n");
+					check(write(fifo,data,strlen(data)),"write");
+					close(fifo);
+					break;
+		//Client
+		case 2:		printf("CLIENT\n");
+
+					check(fifo=open("FIFO",O_WRONLY),"open");
+					printf("Requesting %s\n",argv[1]);
+					check(write(fifo,argv[1],strlen(argv[1])),"write");
+					close(fifo);
+
+					sleep(1);
+					check(mkfifo("FIFO",S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO),"mkfifo");
+					check(fifo=open("FIFO",O_RDONLY|O_NONBLOCK),"open");
+					printf("Waiting for server...\n");
+					while((count=read(fifo,data,sizeof(data)))==0)	sleep(1);
+					data[count]='\0';
+					printf("DATA:\n%s\n",data);
+					close(fifo);
+					unlink("FIFO");
+					break;
+		default:	perror("Invalid no. of arguments!\n");
+					exit(1);
+	}
+
+	return 0;
+}
---
+++ (10CSL77) Networks Lab/10 - Client Server FIFO/README.md
@@ -0,0 +1,1 @@
+# 10. Using FIFOs, write a client-server program to make the client send the file name and to make the server send back the contents of the file if present.
